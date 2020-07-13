add5d36119a51c056659ca7e13a304ac31b83cc0
raymond-devries <raymond.l.devries@gmail.com>
2020-07-12 23:20:28 -0700

    Added combining multiple results to get all requested data.

--- polygon_cache/cache.py
+++ polygon_cache/cache.py
@@ -1,7 +1,12 @@
+from typing import List
+
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
+
 from polygon import RESTClient
+from polygon.rest.models import StocksEquitiesAggregatesApiResponse
 import requests_cache
 import requests
-from datetime import datetime
 import pytz
 
 
@@ -30,7 +35,7 @@ class CachedRESTClient(RESTClient):
         return False
 
     @staticmethod
-    def _filter_by_from(parsed_response):
+    def _filter_by_from(parsed_response: dict):
         # all polygon api requests that use a
         # singular historical date use this format
         return datetime.strptime(parsed_response['from'],
@@ -38,9 +43,93 @@ class CachedRESTClient(RESTClient):
             pytz.timezone('EST')).date()
 
     @staticmethod
-    def _filter_by_unix_timestamp(parsed_response):
+    def _filter_by_unix_timestamp(parsed_response: dict):
         # aggregate results and historic quotes
         # that need to be cached use this format
         return datetime.utcfromtimestamp(
             parsed_response['results'][-1]['t'] / 1000).date() < datetime.now(
             pytz.UTC).date()
+
+    def stocks_equities_aggregates(self, ticker, multiplier, timespan, from_, to,
+                                   max_threads=20, **query_params
+                                   ) -> StocksEquitiesAggregatesApiResponse:
+        start = datetime.strptime(from_, '%Y-%m-%d')
+        end = datetime.strptime(to, '%Y-%m-%d')
+        if timespan == 'minute' or timespan == 'hour':
+            max_days_calls = 5
+        else:
+            max_days_calls = 3000
+
+        dates_api_calls = self._calculate_aggregate_api_calls(start, end,
+                                                              max_days_calls)
+
+        executor = ThreadPoolExecutor(max_threads)
+        api_responses = []
+        for dates in dates_api_calls:
+            date1 = dates[0].strftime('%Y-%m-%d')
+            date2 = dates[1].strftime('%Y-%m-%d')
+            api_responses.append(executor.submit(
+                super().stocks_equities_aggregates, ticker, multiplier, timespan,
+                date1, date2))
+
+        api_responses = [result.result() for result in api_responses]
+        return self._combine_aggregate_results(api_responses)
+
+    @staticmethod
+    def _calculate_aggregate_api_calls(start: datetime, end: datetime,
+                                       days: int) -> List[tuple]:
+        current_day = start
+        period = timedelta(days=days)
+        dates = []
+        while current_day <= end:
+            value1 = current_day
+            next_date = current_day + period
+            if end > next_date:
+                value2 = next_date
+            else:
+                value2 = end
+            dates.append((value1, value2))
+            current_day = value2 + timedelta(1)
+
+        return dates
+
+    @staticmethod
+    def _combine_aggregate_results(
+            api_responses: List[StocksEquitiesAggregatesApiResponse]):
+        first_result = api_responses[0]
+        expected_ticker = first_result.ticker
+        expected_status = first_result.status
+        expected_adjusted = first_result.adjusted
+        query_count_combined = 0
+        results_count_combined = 0
+        results_combined = []
+        for api_response in api_responses:
+            if api_response.ticker != expected_ticker:
+                raise ValueError(
+                    f'Multiple tickers encountered while trying to combine results: '
+                    f'{api_response.ticker} and {expected_ticker}')
+            
+            if api_response.status != expected_status:
+                raise ValueError(
+                    f'Multiple statuses encountered while trying to combine results: '
+                    f'{api_response.status} and {expected_status}')
+
+            if api_response.adjusted != expected_adjusted:
+                raise ValueError(
+                    f'Multiple adjusted values encountered while trying to '
+                    f'combine results: {api_response.adjusted} and {expected_adjusted}')
+
+            query_count_combined += api_response.queryCount
+            results_count_combined += api_response.resultsCount
+            results_combined += api_response.results
+
+        combined_api_response = StocksEquitiesAggregatesApiResponse()
+        combined_api_response.ticker = expected_ticker
+        combined_api_response.status = expected_status
+        combined_api_response.adjusted = expected_adjusted
+        combined_api_response.queryCount = query_count_combined
+        combined_api_response.resultsCount = results_count_combined
+        combined_api_response.results = results_combined
+
+        return combined_api_response
+
--- polygon_cache/tests/test_cache.py
+++ polygon_cache/tests/test_cache.py
@@ -1,8 +1,18 @@
 from polygon_cache.cache import CachedRESTClient
+from polygon_cache.tests import expected_values
+from datetime import datetime
 import pytest
 import requests
 import responses
 import os
+from polygon.rest.models import StocksEquitiesAggregatesApiResponse
+from polygon import RESTClient
+
+
+@pytest.fixture
+def create_client(tmpdir):
+    temp = str(tmpdir.join('polygon-cache'))
+    return CachedRESTClient('api_key', cache_location=temp)
 
 
 @pytest.fixture
@@ -47,3 +57,126 @@ def test_cache_filter_unix_timestamps(unix_ms, expected_filter_response,
     data = {'results': [{'t': 'not this one'}, {'t': unix_ms}]}
     resp, client = fake_json_request_on_2020_01_17(data)
     assert client._cache_filter(resp) is expected_filter_response
+
+
+@pytest.mark.parametrize('day1,day2,interval,expected_dates',
+                         [(datetime(2020, 1, 1), datetime(2020, 1, 14), 5,
+                           [(datetime(2020, 1, 1, 0, 0), datetime(2020, 1, 6, 0, 0)),
+                            (datetime(2020, 1, 7, 0, 0), datetime(2020, 1, 12, 0, 0)),
+                            (datetime(2020, 1, 13, 0, 0), datetime(2020, 1, 14, 0, 0))]
+                           ),
+                          (datetime(2020, 1, 1), datetime(2020, 1, 14), 3000,
+                           [(datetime(2020, 1, 1, 0, 0), datetime(2020, 1, 14, 0, 0))]
+                           )
+                          ])
+def test_calculate_aggregate_api_calls(day1, day2, interval, expected_dates,
+                                       create_client):
+    client = create_client
+    assert client._calculate_aggregate_api_calls(day1, day2, interval) == expected_dates
+
+
+def create_fake_aggv2_results(number_of_results: int = 50,
+                              ticker_symbol: str = 'TIC',
+                              volume_initial: int = 1000,
+                              open_initial: float = 100,
+                              close_initial: float = 105,
+                              high_initial: float = 110,
+                              low_initial: float = 95,
+                              unix_msec_timestamp_initial: int = 100000,
+                              number_of_items_in_aggregate_window: float = 5):
+    results = []
+    for i in range(0, number_of_results):
+        result = {
+            'T': ticker_symbol,
+            'v ': volume_initial + i,
+            'o': open_initial + i,
+            'c': close_initial + i,
+            'h': high_initial + i,
+            'l': low_initial + i,
+            't': unix_msec_timestamp_initial + i * 10,
+            'n': number_of_items_in_aggregate_window + i}
+        results.append(result)
+
+    return results
+
+
+@pytest.fixture
+def create_fake_stocks_equities_aggregate_api_response():
+    def _create_fake_stocks_equities_aggregate_api_response(ticker: str = 'TIC',
+                                                            status: str = 'OK',
+                                                            adjusted: bool = True,
+                                                            query_count: int = 100,
+                                                            results_count: int = 50,
+                                                            results=
+                                                            create_fake_aggv2_results):
+        api_response = StocksEquitiesAggregatesApiResponse()
+        api_response.ticker = ticker
+        api_response.status = status
+        api_response.adjusted = adjusted
+        api_response.queryCount = query_count
+        api_response.resultsCount = results_count
+        api_response.results = results()
+
+        return api_response
+
+    return _create_fake_stocks_equities_aggregate_api_response
+
+
+def test_aggregate_call(mocker, create_fake_stocks_equities_aggregate_api_response,
+                        create_client):
+    mock = mocker.patch.object(RESTClient, 'stocks_equities_aggregates')
+    mock.return_value = create_fake_stocks_equities_aggregate_api_response()
+    client = create_client
+    combined_results = client.stocks_equities_aggregates('TIC', 1, 'minute',
+                                                         '2020-06-04',
+                                                         '2020-06-20')
+
+    assert combined_results.__dict__ == expected_values.AGGREGATES_TEST
+
+
+@pytest.mark.parametrize('attr,values,error_msg',
+                         [('ticker', ['TIC1', 'TIC2'],
+                           'Multiple tickers encountered while trying to '
+                           'combine results: TIC2 and TIC1'
+                           ),
+                          ('status', ['OK', 'NOT_OK'],
+                           'Multiple statuses encountered while trying to '
+                           'combine results: NOT_OK and OK'
+                           ),
+                          ('status', [True, False],
+                           'Multiple statuses encountered while trying to '
+                           'combine results: False and True'
+                           ),
+                          ])
+def test_value_error_multiple_tickers_combine_aggregate_results(
+        create_fake_stocks_equities_aggregate_api_response, attr, values, error_msg,
+        create_client):
+    client = create_client
+    api_responses = [
+        create_fake_stocks_equities_aggregate_api_response(**{attr: value})
+        for value in values]
+
+    with pytest.raises(ValueError) as error:
+        client._combine_aggregate_results(api_responses)
+
+    assert error_msg in str(error)
+
+
+@pytest.mark.parametrize('attr,values,expected_response',
+                         [('queryCount', [20, 30, 50], 100),
+                          ('resultsCount', [10, 20, 35], 65),
+                          ('results',
+                           [create_fake_aggv2_results, create_fake_aggv2_results],
+                           expected_values.COMBINED_RESULTS)])
+def test_combine_aggregate_results(
+        create_fake_stocks_equities_aggregate_api_response, attr, values,
+        expected_response, create_client):
+    client = create_client
+    api_responses = [
+        create_fake_stocks_equities_aggregate_api_response(**{attr: value})
+        for value in values]
+
+    combined = \
+        client._combine_aggregate_results(api_responses)
+
+    assert getattr(combined, attr) == expected_response
